c584a95ae0bcd16065b5f345293eef1b7bab5739
Vinnydaninja <vincenguyen35@gmail.com>
2022-12-12 22:55:26 -0800

    First steps to learning React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const title = "Welcome to the new blog";
+    const likes = 50;
+    const link = "http://www.google.com";
+
+    return (
+        <div className="App">
+            <div className="content">
+                <h1>{ title }</h1>
+                <p>Liked { likes } times</p>
+
+                {/* <p></p> */}
+
+                <p>{ 10 }</p>
+                <p>{ "hello, ninjas" }</p>
+                <p>{ [1,2,3,4,5] }</p>
+                <p>{ Math.random() * 10 }</p>
+
+                <a href={ link }>Google Site</a>
+            </div>
+        </div>
+    );
 }
 
 export default App;
