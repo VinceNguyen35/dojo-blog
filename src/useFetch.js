d77abc07043ec92ae207a5c687ba344d9e606c86
Vinnydaninja <vincenguyen35@gmail.com>
2022-12-15 00:48:24 -0800

    Cleanup handling for abort fetch

--- src/useFetch.js
+++ src/useFetch.js
@@ -6,8 +6,10 @@ const useFetch = (url) => {
     const [error, setError] = useState(null);
 
     useEffect(() => {
+        const abortCont = new AbortController();
+
         setTimeout(() => {
-            fetch(url)
+            fetch(url, { signal: abortCont.signal })
             .then(res => {
                 if(!res.ok) {
                     throw Error("Could not fetch the data for that resource");
@@ -20,9 +22,15 @@ const useFetch = (url) => {
                 setError(null);
             })
             .catch(err => {
-                setIsPending(false);
-                setError(err.message);
+                if(err.name === "AbortError") {
+                    console.log("fetch aborted");
+                } else {
+                    setIsPending(false);
+                    setError(err.message);
+                }
             });
+
+            return () => abortCont.abort();
         }, 1000);
     }, [url]);
 
