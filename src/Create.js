9b1771dc44756c83f8f990e9fc4a78b32b522ff3
Vinnydaninja <vincenguyen35@gmail.com>
2022-12-16 01:31:32 -0800

    Add redirect back to home page after adding blog

--- src/Create.js
+++ src/Create.js
@@ -1,10 +1,12 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Create = () => {
     const [title, setTitle] = useState("");
     const [body, setBody] = useState("");
     const [author, setAuthor] = useState("");
     const [isPending, setIsPending] = useState(false);
+    const navigate = useNavigate();
 
     const handleSubmit = (event) => {
         event.preventDefault();
@@ -20,6 +22,7 @@ const Create = () => {
         .then(() => {
             console.log("New blog added");
             setIsPending(false);
+            navigate("/");
         });
     }
 
